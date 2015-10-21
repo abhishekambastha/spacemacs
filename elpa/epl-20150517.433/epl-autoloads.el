21df945fb882b70223636de1563acf9f57b4656c
Abhishek Ambastha <ambastha.abhishek@gmail.com>
2015-10-21 08:35:16 -0700

    Modified Theme variables

---
+++ elpa/epl-20150517.433/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epl.el") (22055 16985 809116 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
